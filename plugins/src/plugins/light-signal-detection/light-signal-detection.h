bb99956d28cfa6c240dd0263033ba2ae5c50c675
ValentinVERGEZ <valentin.vergez@gmail.com>
2015-10-10 17:38:49 +0200

    Remise en page du code pour relecture

--- plugins/src/plugins/light-signal-detection/light-signal-detection.h
+++ plugins/src/plugins/light-signal-detection/light-signal-detection.h
@@ -44,58 +44,58 @@ typedef const boost::shared_ptr<llsf_msgs::MachineInfo const> ConstMachineInfoPt
 
 namespace gazebo
 {
-  /**
-   * Provides ground Truth position
-   * @author Frederik Zwilling
-   */
-  class LightSignalDetection : public ModelPlugin
-  {
-  public:
-    LightSignalDetection();
-   ~LightSignalDetection();
+	/**
+	 * Provides ground Truth position
+	 * @author Frederik Zwilling
+	 */
+	class LightSignalDetection : public ModelPlugin
+	{
+		public:
+			LightSignalDetection();
+			~LightSignalDetection();
 
-    //Overridden ModelPlugin-Functions
-    virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
-    virtual void OnUpdate(const common::UpdateInfo &);
-    virtual void Reset();
+			//Overridden ModelPlugin-Functions
+			virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+			virtual void OnUpdate(const common::UpdateInfo &);
+			virtual void Reset();
 
-  private:
-    /// Pointer to the gazbeo model
-    physics::ModelPtr model_;
-    /// Pointer to the update event connection
-    event::ConnectionPtr update_connection_;
-    ///Node for communication to fawkes
-    transport::NodePtr node_;
-    ///Node for communication in gazebo
-    transport::NodePtr world_node_;
-    ///name of the communication channel and the sensor
-    std::string name_;
+		private:
+			/// Pointer to the gazbeo model
+			physics::ModelPtr model_;
+			/// Pointer to the update event connection
+			event::ConnectionPtr update_connection_;
+			///Node for communication to fawkes
+			transport::NodePtr node_;
+			///Node for communication in gazebo
+			transport::NodePtr world_node_;
+			///name of the communication channel and the sensor
+			std::string name_;
 
-    ///time variable to send in intervals
-    double last_sent_time_;
+			///time variable to send in intervals
+			double last_sent_time_;
 
-    //Light-detection Stuff:
-    ///Functions for sending information to fawkes:
-    void send_light_detection();
+			//Light-detection Stuff:
+			///Functions for sending information to fawkes:
+			void send_light_detection();
 
-    //remember light state in front of the robot
-    llsf_msgs::LightState state_red_, state_yellow_, state_green_;
-    /// Subscriber to get msgs about the light status
-    transport::SubscriberPtr light_msg_sub_;
-    /// Handler for light status msg
-    void on_light_msg(ConstMachineInfoPtr &msg);
-    void save_light_signal(llsf_msgs::Machine machine);
+			//remember light state in front of the robot
+			llsf_msgs::LightState state_red_, state_yellow_, state_green_;
+			/// Subscriber to get msgs about the light status
+			transport::SubscriberPtr light_msg_sub_;
+			/// Handler for light status msg
+			void on_light_msg(ConstMachineInfoPtr &msg);
+			void save_light_signal(llsf_msgs::Machine machine);
 
-    //is the light currently detected?
-    bool visible_;
-    //how long was the light detected?
-    int visibility_history_;
-    double visible_since_;
+			//is the light currently detected?
+			bool visible_;
+			//how long was the light detected?
+			int visibility_history_;
+			double visible_since_;
 
-    //robot position
-    math::Pose robot_pose_;
+			//robot position
+			math::Pose robot_pose_;
 
-    ///Publisher for Detected light signal
-    transport::PublisherPtr light_signal_pub_;
-  };
+			///Publisher for Detected light signal
+			transport::PublisherPtr light_signal_pub_;
+	};
 }
